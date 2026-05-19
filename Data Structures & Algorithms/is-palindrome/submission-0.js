bbd8010d9629c238fe8b4cc06de31b8a3058f21f
Auri_Arevalo <129828838+Stifung@users.noreply.github.com>
2026-05-18 22:19:43 -0600

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isPalindrome(s){
+        if(!s || s.length <= 1){
+            return true;
+        }
+        let left = 0;
+        let right = s.length -1;
+        while(left < right){
+            while(left < right && !this.isAlphaNumeric(s.charCodeAt(left))){
+                left ++;
+            }
+            while(left < right && !this.isAlphaNumeric(s.charCodeAt(right))){
+                right --;
+            }
+            if(s[left].toLowerCase() !== s[right].toLowerCase()){
+                return false;
+            }
+            left ++;
+            right --;
+        }
+        return true;
+    }
+
+    /**
+     * @param {number} code
+     * @return {boolean}
+     */
+        isAlphaNumeric(code){
+        return(
+            (code >= 48 && code <= 57) ||
+            (code >= 65 && code <= 90) ||
+            (code >= 97 && code <= 122)
+        );
+    }
+}
